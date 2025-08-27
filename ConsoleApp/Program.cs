6a008be88f1bcffb783e619f547f70863655ea47
Andreea Anghel <94113632+andreeaang01@users.noreply.github.com>
2025-08-27 23:19:18 +0300

    Create the menu (#7)
    Added start menu logic

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            StartMenu();
+        }
+
+        protected static void StartMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Please type one of the following numbers that corresponds to the option you chose.");
+                Console.WriteLine(" ");
+                Console.WriteLine("[1] Generate password");
+                Console.WriteLine("[2] Add new account");
+                Console.WriteLine("[3] Show accounts");
+
+                Console.WriteLine(" ");
+
+                string userInput = Console.ReadLine() ?? "-1";
+
+                Console.WriteLine(" ");
+
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine("This feature is not implemented yet :(");
+                        break;
+
+                    case "2":
+                        Console.WriteLine("This feature is not implemented yet :(");
+                        break;
+
+                    case "3":
+                        Console.WriteLine("This feature is not implemented yet :(");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid option :(");
+                        break;
+                }
+            }
         }
     }
 }
